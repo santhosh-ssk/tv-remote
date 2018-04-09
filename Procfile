daa8347e98a3a8ae1c1d2e7977e75e115bca5988
santhosh <santhoshkumar.ssk3222@gmail.com>
2018-04-09 12:43:53 +0530

    changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-class socketio.sgunicorn.GeventSocketIOWorker --log-file=- server:app
+gunicorn --worker-class eventlet -w 1 module:app
--- server.py
+++ server.py
@@ -1,43 +1,19 @@
 from flask import Flask, render_template
-from flask.ext.socketio import SocketIO, emit,send
-from flask_socketio import join_room, leave_room
-
-
+from flask.ext.socketio import SocketIO
+import json
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-app.config['SECRET_KEY'] = 'secret!'
-values = {
-    'slider1': 25,
-    'slider2': 0,
-}
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', **values)
+    return render_template('index.html',)
 
-@socketio.on('join')
-def on_join(data):
-    print(data)
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
+@socketio.on('send_message')
+def handle_source(json_data):
+    text = json_data['message'].encode('ascii', 'ignore')
+    socketio.emit('echo', {'echo': 'Server Says: '+text})
 
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    send(username + ' has left the room.', room=room)
-
-@socketio.on('my event')
-def my_event(message):
-	print(message)
-
-@socketio.on('device_registration')
-def device_registration(data):
-	print(data)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app)
+
