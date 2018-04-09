ab16419d7ddf734e8eed60e877f88a33c1de30e2
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-04-09 11:50:02 +0530

    second

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-from flask.ext.socketio import SocketIO, emit
+from flask.ext.socketio import SocketIO, emit,send
+from flask_socketio import join_room, leave_room
+
 
 
 app = Flask(__name__)
@@ -14,12 +16,28 @@ values = {
 def index():
     return render_template('index.html', **values)
 
-@socketio.on('client1')
-def test_message(message):                    
-    emit('client2', {'data': 'testdata'})
+@socketio.on('join')
+def on_join(data):
+    print(data)
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
 @socketio.on('my event')
 def my_event(message):
 	print(message)
 
+@socketio.on('device_registration')
+def device_registration(data):
+	print(data)
+
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1',port=5000)
+    socketio.run(app, host='127.0.0.1',port=5001)
